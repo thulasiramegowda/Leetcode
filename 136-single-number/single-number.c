a5adf128aec05256d02088cdaa8cc3b9637f0bf9
Thulasi G T <thulasiramegowda29@gmail.com>
2026-06-28 21:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (68.46%) - LeetSync

---
+++ 136-single-number/single-number.c
@@ -0,0 +1,8 @@
+int singleNumber(int* nums, int numsSize) {
+   int result=0;
+   for(int i=0;i<numsSize;i++){
+    result=result^nums[i];
+    //^(XOR)->used to element pair numbers 
+   } 
+   return result;
+}
