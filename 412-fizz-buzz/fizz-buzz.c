e7ea72f3fad3d29f05bd60a4c52b75fe3b72d8fd
Thulasi G T <thulasiramegowda29@gmail.com>
2026-09-22 18:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (39.43%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char** fizzBuzz(int n, int* returnSize) {
+
+    *returnSize = n;
+
+    char **answer = malloc(n * sizeof(char *));
+
+    for (int i = 1; i <= n; i++) {
+
+        // Allocate space for the string at answer[i - 1]
+        answer[i - 1] = malloc(20 * sizeof(char));
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            sprintf(answer[i - 1], "FizzBuzz");
+        }
+        else if (i % 3 == 0) {
+            sprintf(answer[i - 1], "Fizz");
+        }
+        else if (i % 5 == 0) {
+            sprintf(answer[i - 1], "Buzz");
+        }
+        else {
+            sprintf(answer[i - 1], "%d", i);
+        }
+    }
+
+    return answer;
+}
