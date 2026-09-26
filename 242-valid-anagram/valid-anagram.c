202c1bf3c689cbeaa149e4a9c91d731912718e4f
Thulasi G T <thulasiramegowda29@gmail.com>
2026-09-26 12:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (67.37%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.c
@@ -0,0 +1,19 @@
+bool isAnagram(char* s, char* t) {
+    int count[26] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        count[s[i] - 'a']++;
+    }
+
+    for (int i = 0; t[i] != '\0'; i++) {
+        count[t[i] - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
